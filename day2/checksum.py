a6554ea35ad13f1abe8555536cd613bce02aaa5d
Jeorry Balasabas <jeorryb@Jeorrys-MacBook-Pro.local>
2018-12-03 09:39:15 -0500

    Day 2 - Box Id's

---
+++ day2/checksum.py
@@ -0,0 +1,23 @@
+import collections
+
+def calcchecksum(inputfile):
+    with open(inputfile, 'r') as f:
+        inputs = [line.strip() for line in f]
+        twochar = set()
+        threechar = set()
+        for input in inputs:
+            for letter in input:
+                if input.count(letter) == 2:
+                    twochar.add(input)
+                if input.count(letter) == 3:
+                    threechar.add(input)
+        
+        x = (len(twochar))
+        y = (len(threechar))
+        checksum = x*y
+        return checksum
+
+
+checksum = calcchecksum('input.txt')
+
+print(f'The checksum is {checksum}')
