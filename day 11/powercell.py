d76ce5acc1cb0f10c5d2238ce58763f865c56a7b
Jeorry Balasabas <jeorryb@Jeorrys-MBP.attlocal.net>
2019-11-12 10:55:40 -0500

    Day 11 Solution

---
+++ day 11/powercell.py
@@ -0,0 +1,89 @@
+import numpy
+import operator
+from numpy.lib.stride_tricks import as_strided
+
+serial = int(3628)
+
+def power(x, y):
+    rack = (x + 1) + 10
+    power = rack * (y + 1)
+    power += serial
+    power *= rack
+    return (power // 100 % 10) - 5
+
+grid = numpy.fromfunction(power, (300, 300))
+
+def sliding_window_view(arr, window_shape, steps):
+    """ Produce a view from a sliding, striding window over `arr`.
+        The window is only placed in 'valid' positions - no overlapping
+        over the boundary.
+        Parameters
+        ----------
+        arr : numpy.ndarray, shape=(...,[x, (...), z])
+            The array to slide the window over.
+        window_shape : Sequence[int]
+            The shape of the window to raster: [Wx, (...), Wz],
+            determines the length of [x, (...), z]
+        steps : Sequence[int]
+            The step size used when applying the window
+            along the [x, (...), z] directions: [Sx, (...), Sz]
+        Returns
+        -------
+        view of `arr`, shape=([X, (...), Z], ..., [Wx, (...), Wz])
+            Where X = (x - Wx) // Sx + 1
+        Notes
+        -----
+        In general, given
+          `out` = sliding_window_view(arr,
+                                      window_shape=[Wx, (...), Wz],
+                                      steps=[Sx, (...), Sz])
+           out[ix, (...), iz] = arr[..., ix*Sx:ix*Sx+Wx,  (...), iz*Sz:iz*Sz+Wz]
+         Examples
+         --------
+         >>> import numpy as np
+         >>> x = np.arange(9).reshape(3,3)
+         >>> x
+         array([[0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8]])
+         >>> y = sliding_window_view(x, window_shape=(2, 2), steps=(1, 1))
+         >>> y
+         array([[[[0, 1],
+                  [3, 4]],
+                 [[1, 2],
+                  [4, 5]]],
+                [[[3, 4],
+                  [6, 7]],
+                 [[4, 5],
+                  [7, 8]]]])
+         """
+    in_shape = numpy.array(arr.shape[-len(steps):])  # [x, (...), z]
+    window_shape = numpy.array(window_shape)  # [Wx, (...), Wz]
+    steps = numpy.array(steps)  # [Sx, (...), Sz]
+    nbytes = arr.strides[-1]  # size (bytes) of an element in `arr`
+
+    # number of per-byte steps to take to fill window
+    window_strides = tuple(numpy.cumprod(arr.shape[:0:-1])[::-1]) + (1,)
+    # number of per-byte steps to take to place window
+    step_strides = tuple(window_strides[-len(steps):] * steps)
+    # number of bytes to step to populate sliding window view
+    strides = tuple(int(i) * nbytes for i in step_strides + window_strides)
+
+    outshape = tuple((in_shape - window_shape) // steps + 1)
+    # outshape: ([X, (...), Z], ..., [Wx, (...), Wz])
+    outshape = outshape + arr.shape[:-len(steps)] + tuple(window_shape)
+    return as_strided(arr, shape=outshape, strides=strides, writeable=False)
+
+windows = sliding_window_view(grid, window_shape=(3, 3), steps=(1, 1))
+
+# window_max = max(numpy.sum(x) for x in windows)
+cell_sums = {}
+
+for x in range(len(windows)):
+    for y in range(len(windows)):
+        cell_sums[(x, y)] = numpy.sum(windows[x, y])
+
+max_loc = max(cell_sums.items(), key=operator.itemgetter(1))[0]
+
+print(max_loc)
+print(windows[max_loc[0], max_loc[1]])
