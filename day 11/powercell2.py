b1449471c49d7c0c04cc9029b34b997906152d1b
Jeorry Balasabas <jeorryb@Jeorrys-MBP.attlocal.net>
2019-11-19 09:14:19 -0500

    Day 12 Solution

--- day 11/powercell2.py
+++ day 11/powercell2.py
@@ -8,10 +8,17 @@ def power(x, y):
     power *= rack
     return (power // 100 % 10) - 5
 
-grid = numpy.fromfunction(power, (300, 300))
+grid = numpy.fromfunction(power, (9, 9))
 
-for width in range(3, 300):
-    windows = sum(grid[x:x-width+1 or None, y:y-width+1 or None] for x in range(width) for y in range(width))
-    maximum = int(windows.max())
-    location = numpy.where(windows == maximum)
-    print(width, maximum, location[0][0] + 1, location[1][0] + 1)
+# for width in range(3, 300):
+#     windows = sum(grid[x:x-width+1 or None, y:y-width+1 or None] for x in range(width) for y in range(width))
+#     maximum = int(windows.max())
+#     location = numpy.where(windows == maximum)
+#     print(width, maximum, location[0][0] + 1, location[1][0] + 1)
+
+windows = sum(grid[x:x-3+1 or None, y:y-3+1 or None] for x in range(3) for y in range(3))
+print(windows)
+maximum = int(windows.max())
+location = numpy.where(windows == maximum)
+
+print(maximum, location)
